2a941fd8ca3daa45e1cd5dd9e679f15a48bc58e2
HangFamily <hangquoctai.family@gmail.com>
2023-03-24 17:38:07 +0700

    Upload link store

--- client/src/features/products/productService.js
+++ client/src/features/products/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/embed/products/";
+const API_URL = "https://hammerhead-app-wjzp7.ondigitalocean.app/embed/products/";
 
 const call_apiProduct = async (options) => {
   const response = await axios.get(API_URL, { params: options });
